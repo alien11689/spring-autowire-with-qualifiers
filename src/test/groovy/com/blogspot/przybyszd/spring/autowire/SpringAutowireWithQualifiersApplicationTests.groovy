8d2554fb64a444995221cf8baf4c451bd140ee2c
Dominik Adam Przybysz <alien11689@gmail.com>
2015-12-07 22:16:08 +0100

    Two implementation with qualifier

--- src/test/groovy/com/blogspot/przybyszd/spring/autowire/SpringAutowireWithQualifiersApplicationTests.groovy
+++ src/test/groovy/com/blogspot/przybyszd/spring/autowire/SpringAutowireWithQualifiersApplicationTests.groovy
@@ -3,6 +3,7 @@ package com.blogspot.przybyszd.spring.autowire
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.SpringApplicationConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 
@@ -11,6 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 class SpringAutowireWithQualifiersApplicationTests {
 
     @Autowired
+    @Qualifier('htmlHeaderPrinter')
     IHeaderPrinter headerPrinter
 
     @Test
