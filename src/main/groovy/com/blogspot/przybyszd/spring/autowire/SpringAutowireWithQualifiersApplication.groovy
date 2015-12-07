ec73cd06dd8610c3d8a67fa56ffe0022c2abf458
Dominik Adam Przybysz <alien11689@gmail.com>
2015-12-07 21:22:58 +0100

    Initial commit

---
+++ src/main/groovy/com/blogspot/przybyszd/spring/autowire/SpringAutowireWithQualifiersApplication.groovy
@@ -0,0 +1,12 @@
+package com.blogspot.przybyszd.spring.autowire
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringAutowireWithQualifiersApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run SpringAutowireWithQualifiersApplication, args
+    }
+}
